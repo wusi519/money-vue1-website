027649811b8d82b9e6e8d794159e98e7c75b0c26
wusi519 <327742525@qq.com>
2021-08-14 17:47:18 +0800

    deploy

---
+++ precache-manifest.c7391e04fa098836007b906fbf8c69ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d07a9f2a52ededb2c143",
+    "url": "/money-vue1-website/css/app.122a9a26.css"
+  },
+  {
+    "revision": "d22d00be969e7422397de4f86bd1a112",
+    "url": "/money-vue1-website/index.html"
+  },
+  {
+    "revision": "d07a9f2a52ededb2c143",
+    "url": "/money-vue1-website/js/app.04270a11.js"
+  },
+  {
+    "revision": "a383f9aa7ce910529f42",
+    "url": "/money-vue1-website/js/chunk-vendors.1391d4fe.js"
+  },
+  {
+    "revision": "b8a7ec4775c57cdc35a0a57699c8a677",
+    "url": "/money-vue1-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-vue1-website/robots.txt"
+  }
+]);
